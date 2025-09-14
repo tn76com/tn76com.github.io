d573b2ba437cdceea03fe4cc3fe28ecebfc333da
Ilyes Tounsi <tounsils@gmail.com>
2025-09-13 22:04:49 -0700

    Updated Pages

---
+++ assets/GetInTouch-D3KNC3Mo.js
@@ -0,0 +1,6 @@
+import{r as s,j as e,L as t}from"./index-CmT9dIld.js";const a=()=>(s.useEffect(()=>{},[]),e.jsxs(e.Fragment,{children:[e.jsx("section",{className:"bg-light py-5 text-center",children:e.jsxs("div",{className:"container",children:[e.jsx("h2",{className:"mb-4",children:"Ready to elevate your digital presence?"}),e.jsx("p",{className:"mb-4",children:"Contact us today and start building innovative, secure, and AI-powered solutions for your business."}),e.jsx(t,{to:"/contact",className:"btn btn-lg glowing-btn",children:"Get in Touch"})]})}),e.jsx("style",{children:`
+          .hover-scale:hover {
+            transform: translateY(-5px);
+            transition: all 0.3s ease;
+          }
+        `})]}));export{a as S};
